519f94237fd0b5a50407b60c1aa2484bc0266a29
clkeuch <54365483+clkeuch@users.noreply.github.com>
2020-01-22 09:50:06 -0600

    These are my new files

---
+++ p5/Week2/trees/sketch.js
@@ -0,0 +1,43 @@
+function setup() {
+  // put setup code here
+  createCanvas(720, 400);
+}
+
+function draw() {
+  // put drawing code here
+ //  fill(204);
+ // triangle(18, 18, 18, 360, 81, 360);
+ //
+ // fill(102);
+ // rect(81, 81, 63, 63);
+ //
+ // fill(204);
+ // quad(189, 18, 216, 18, 216, 360, 144, 360);
+ //
+ // fill(255);
+ // ellipse(252, 144, 72, 72);
+ //
+ // fill(204);
+ // triangle(288, 18, 351, 360, 288, 360);
+ //
+ // fill(255);
+ // arc(479, 300, 280, 280, PI, TWO_PI);
+background (111, 144, 242)
+stroke(0);
+ fill(252, 186, 3);
+  ellipse(100, 75, 85, 85);
+
+  fill(189, 154, 79);
+  rect(550, 75, 50, 300);
+
+  fill(103, 189, 79);
+  ellipse(570, 75, 100, 100);
+noStroke();
+  fill(255);
+  ellipse(290, 75, 110, 85);
+
+  fill(255);
+  ellipse(320, 75, 110, 85);
+
+
+}
