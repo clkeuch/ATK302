62e6fec01c3c42358bef80c30b8115fa300a43a9
clkeuch <54365483+clkeuch@users.noreply.github.com>
2020-02-25 11:33:24 -0600

    multsounds

--- p5/Week7/joke1/sketch.js
+++ p5/Week7/joke1/sketch.js
@@ -1,7 +1,35 @@
+var myState = 0;
+
+function preload() {
+
+}
+
 function setup() {
   // put setup code here
+  createCanvas(800, 800);
 }
 
 function draw() {
   // put drawing code here
+  background('black');
+
+
+  switch (myState) {
+    case 0:
+      text("Why do we tell actors to break a leg?", 100, 100);
+      break;
+
+    case 1:
+      text("Because every play has a cast!", 100, 100);
+      break;
+
+  }
+}
+
+
+function mouseReleased() {
+  myState = myState + 1;
+  if (myState > 1) {
+    myState = 0;
+  }
 }
---
+++ p5/Week7/joke2/sketch.js
@@ -0,0 +1,47 @@
+var myState = 0;
+var timer = 0
+
+
+function preload() {
+
+}
+
+function setup() {
+  // put setup code here
+  createCanvas(800, 800)
+}
+
+function draw() {
+  // put drawing code here
+  background('black');
+
+
+  switch (myState) {
+    case 0:
+      text("Why do we tell actors to break a leg?", 100, 100) ;
+      timer++ ;
+      if (timer > 200){
+        myState = 1 ;
+        timer = 0 ;
+      }
+      break;
+
+    case 1:
+      text("Because every play has a cast!", 100, 100) ;
+      timer ++ ;
+      if (timer > 200){
+        myState = 0;
+        timer = 0;
+      }
+      break;
+
+  }
+}
+
+
+function mouseReleased() {
+  myState = myState + 1;
+  if (myState > 1) {
+    myState = 0;
+  }
+}
