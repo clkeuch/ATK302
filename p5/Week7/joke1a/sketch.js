62d5f70e217fea5733e22ca9917f1e5ebfe54893
clkeuch <54365483+clkeuch@users.noreply.github.com>
2020-02-27 11:41:49 -0600

    joke 1
    joke

--- p5/Week7/joke1a/sketch.js
+++ p5/Week7/joke1a/sketch.js
@@ -1,4 +1,7 @@
 var myState = 0;
+function preload (){
+
+}
 
 function setup() {
   // put setup code here
@@ -13,21 +16,22 @@ function draw() {
     case 0:
     fill("white");
     text("Why do we tell actors to break a leg?", 100, 100) ;
-    timer ++ ;
-    if (timer > 200){
-      myState = 1 ;
-      timer = 0 ;
-    }
-    break ;
-
-    case 1:
-    text("Because every play has a cast!", 100, 100) ;
-    timer ++ ;
-    if (timer > 200){
-      myState = 0 ;
-    }
     break;
 
 
+
+    case 1:
+    text("Because every play has a cast!", 100, 100) ;
+    break;
+
+
+  }
+}
+
+
+function mouseReleased() {
+  myState = myState + 1;
+  if (myState > 1) {
+    myState = 0;
   }
 }
